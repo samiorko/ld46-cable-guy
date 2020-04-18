147b35e5ccaf1d758f51965c1868e013e3db5873
Sami Orko <sami.orko@gmail.com>
2020-04-18 09:07:14 +0300

    Project template

---
+++ Assets/Rope2D.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope2D : MonoBehaviour
+{
+
+    public Rigidbody2D start;
+    public Rigidbody2D end;
+
+    public int partCount;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        var partVector = (end.position - start.position) / partCount;
+        for (var i = 0; i < partCount; i++)
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
