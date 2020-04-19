a85f5e703c21b8fcba31931bfecc5243acf7c21b
Sami Orko <sami.orko@gmail.com>
2020-04-19 03:20:10 +0300

    Title banner

--- Assets/IntroBanner.cs
+++ Assets/IntroBanner.cs
@@ -1,18 +1,78 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class IntroBanner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public RectTransform m_background;
+
+    public float m_targetHeight;
+    public float m_targetWidth;
+    public float m_durationWidth;
+    public float m_durationHeight;
+
+    public string m_header;
+    public string m_subHeader;
+
+    public Text m_headerTextComponent;
+    public Text m_subHeaderTextComponent;
+
+    private void OnEnable()
     {
-        
+        Show(m_header, m_subHeader);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Show(string header, string subHeader)
     {
-        
+        m_header = header;
+        m_subHeader = subHeader;
+
+        m_headerTextComponent.text = header;
+        m_subHeaderTextComponent.text = subHeader;
+
+        m_background.transform.localScale = Vector2.zero;
+        m_background.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, m_targetHeight);
+        m_background.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, m_targetWidth);
+
+        StartCoroutine(nameof(Open));
+    }
+
+    public IEnumerator Open()
+    {
+
+        yield return new WaitForSeconds(2f);
+        var elapsed = 0f;
+        var tWidth = 0f;
+        var tHeight = 0f;
+
+        while (tWidth < 1f || tHeight < 1f)
+        {
+            elapsed += Time.deltaTime;
+            tHeight = Mathf.Clamp01(elapsed / m_durationHeight);
+            tWidth = Mathf.Clamp01(elapsed / m_durationWidth);
+
+            m_background.transform.localScale = new Vector2(tWidth, tHeight);
+            yield return null;
+        }
+
+        yield return  new WaitForSeconds(5f);
+        yield return StartCoroutine(nameof(Close));
+    }
+
+    public IEnumerator Close()
+    {
+        var elapsed = 0f;
+        var tWidth = 0f;
+        var tHeight = 0f;
+
+        while (tWidth < 1f || tHeight < 1f)
+        {
+            elapsed += Time.deltaTime;
+            tHeight = Mathf.Clamp01(elapsed / m_durationHeight);
+            tWidth = Mathf.Clamp01(elapsed / m_durationWidth);
+
+            m_background.transform.localScale = new Vector2(1f - tWidth,  1f - tHeight);
+            yield return null;
+        }
     }
 }
--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -10,14 +10,31 @@ public class FollowPlayer : MonoBehaviour
     public float m_cameraDistance;
     private Vector2 m_currentVelocity;
 
+    public Transform m_boundsMin;
+    public Transform m_boundsMax;
+
     public void LateUpdate()
     {
-        var target = m_player.position;
+        var target = ClampVector(m_player.position, m_boundsMin.position, m_boundsMax.position);
 
         transform.position =
             Vector2.SmoothDamp(transform.position, target, ref m_currentVelocity, m_smoothTime, m_maxSpeed);
 
         transform.position += Vector3.back * m_cameraDistance;
+
+    }
+
+    private Vector2 ClampVector(Vector2 value, Vector2 min, Vector2 max)
+    {
+        var x = value.x;
+        var y = value.y;
+
+        if (x < min.x) x = min.x;
+        if (x > max.x) x = max.x;
+        if (y < min.y) y = min.y;
+        if (y > max.y) y = max.y;
+
+        return new Vector2(x, y);
 
     }
 }
