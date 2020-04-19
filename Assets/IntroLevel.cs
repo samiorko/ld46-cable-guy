2b2bf6f851a771441d2d873542075db0f85ee6b0
Sami Orko <sami.orko@gmail.com>
2020-04-20 01:31:23 +0300

    Intro and webgl

---
+++ Assets/IntroLevel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroLevel : MonoBehaviour
+{
+    public Text m_text;
+
+    private void Start()
+    {
+        m_text.text = m_text.text.Replace("%CURRENT_DATE%", DateTime.Now.ToString("dd.MM.yyyy"));
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Tutorial");
+        }
+    }
+
+}
